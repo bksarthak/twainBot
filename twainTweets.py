0ba8027d238231aaff1ea98e84b67bbdaa6ffe3d
BK Sarthak Das <bksarthak@users.noreply.github.com>
2015-02-20 00:23:09 -0800

    Update twainTweets.py
    updated with try block

--- twainTweets.py
+++ twainTweets.py
@@ -2,32 +2,35 @@
 
 import tweepy, time, sys
 def tweet(): 
- twainfile = str(sys.argv[1])
- 
+ try:
 #Enter your keys and tokens
- CONSUMER_KEY = [consumerkey]
- CONSUMER_SECRET = [consumersecret]
- ACCESS_TOKEN = [accesstoken]
- ACCESS_SECRET = [accesssecret]
- auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
- auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
- api = tweepy.API(auth)
- 
- filename=open(twainfile,'r')
- f=filename.readlines()
- filename.close()
- 
- for line in f: 
-	 if len(line) < 140: #verify quote is lesser than 140 char limit
-	  if len(line) < 130: #check if the quote has space for hashtag
-	   line = line + '#marktwain'
-	   api.update_status(line)
-	   time.sleep(86400) #tweet every 24 hours
+  twainfile = str(sys.argv[1])
+  CONSUMER_KEY = '50fnMtCR3HN1kVPTg1yI9YgIz'
+  CONSUMER_SECRET = '4oBo1TzfNkSBSolSODRVQr3aAWoacgCxCYvEdunYP6JUtJEBA3'
+  ACCESS_TOKEN = '3032350844-fN2gKOp69TC6qzzx3rGpeiR11N07cGSlvOg41be'
+  ACCESS_SECRET = 'vS9IqJaIWME799cXbQdrD2Glrzoe7PKh8CCnaStD7qSdA'
+  auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+  auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+  api = tweepy.API(auth)
+  
+  filename=open(twainfile,'r')
+  f=filename.readlines()
+  filename.close()
+  
+  for line in f:  
+	  if len(line) < 140: #verify quote is lesser than 140 char limit
+	   if len(line) < 130: #check if the quote has space for hashtag
+	    line = line + '#marktwain'
+	    api.update_status(line)
+	    time.sleep(86400) #tweet every 24 hours
+	   else:
+	    api.update_status(line)
+	    time.sleep(86400)
 	  else:
-	   api.update_status(line)
-	   time.sleep(86400)
-	 else:
-	  line.strip()
+	   line.strip()
+ except tweepy.TweepError as e:
+  print("Tweet not posted ",e) 
+ 
 def main():
  tweet()
 #boilerplate
